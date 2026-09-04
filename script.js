ccacbd52efabd18da249d566e59a432575b61766
smithika838-cpu <smithika838@gmail.com>
2026-09-04 19:04:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -19,6 +19,29 @@ const answerBox=
   document.createElement('div');
 answerBox.style.marginTop='20px';
   answerBox.style.padding='15px';
+document.body.appendChild(answerBox):
+const doubtInput=document.quarySelector('textarea');
+const getAnswerButton=[...document.quarySelectorAll('button)].find(b=>b.textcontent.trim()==='Get Answer);
+//Create the answer box
+const answerBox =document.createElement('div');
+document.body.appendChild(answerBox);
+//WHEN "Get Answer" button is clicked
+getAnswerButton.addEventListen('click', async ()=>{
+  answerBox.innerText="Thinking...";
+  const res=await fetch("https://api.featherless.ai/v1/chat/completions",{
+    method: "POST",
+    headers:{
+      "content-Type": "application/json",
+      "Authorization": "Bearer YOUR_API_KEY_HERE"
+    },
+    body: JSON.stringify({
+      model:"meta-llama/Meta-Llama-3-8B-Instuct",
+      messages: [{ role: "user", content: doubtInput.value }]
+    })
+  });
+  const data = await res.json();
+  answerBox.innerText = data.choices[0].message.content;
+});
 answerBox.style.borderRadius=
   '15px';
 answerBox.style.background='#fff';
